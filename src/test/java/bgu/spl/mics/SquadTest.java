d9e19c6b5a70f5cfa9203f6775286aafececf4c9
ZivSini <ziv264@gmail.com>
2019-12-09 19:23:29 +0200

    ziv test

--- src/test/java/bgu/spl/mics/SquadTest.java
+++ src/test/java/bgu/spl/mics/SquadTest.java
@@ -1,14 +1,23 @@
 package bgu.spl.mics;
 
+import bgu.spl.mics.application.passiveObjects.Agent;
+import bgu.spl.mics.application.passiveObjects.Squad;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class SquadTest {
+    private Squad sq;
     @BeforeEach
     public void setUp(){
+        sq=new Squad();
+        Agent a = new Agent();
+        Agent b = new Agent();
+        Agent c = new Agent();
 
+        Agent[] s = {a,b,c};
+        sq.load(s);
     }
 
     @Test
