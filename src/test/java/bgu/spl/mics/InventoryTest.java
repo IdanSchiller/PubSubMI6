89fc7469768599070b0a0b68dde99cdcd86b91b7
IdanSchiller <idan.schiller@gmail.com>
2019-12-09 19:06:39 +0200

    test

--- src/test/java/bgu/spl/mics/InventoryTest.java
+++ src/test/java/bgu/spl/mics/InventoryTest.java
@@ -1,18 +1,28 @@
 package bgu.spl.mics;
 
+import bgu.spl.mics.application.passiveObjects.Inventory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class InventoryTest {
     @BeforeEach
     public void setUp(){
+        Inventory inv = new Inventory();
 
     }
-    // tes
+
+    //@Test
+    public void testGetInstance(){
+
+    }
+
+
+
     @Test
     public void test(){
+        testGetInstance();
         //TODO: change this test and add more tests :)
         fail("Not a good test");
     }
