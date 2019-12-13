24b53a910684bc6158480a5614df107b40253314
IdanSchiller <idan.schiller@gmail.com>
2019-12-13 23:02:32 +0200

    test

--- src/test/java/bgu/spl/mics/MessageBrokerTest.java
+++ src/test/java/bgu/spl/mics/MessageBrokerTest.java
@@ -6,11 +6,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MessageBrokerTest {
+
+    Event event;// = new MissionRecievedEvent();
+    Subscriber simlpeSub;// = new SimpleSubscriber();
+    String resault = "Not Resolved";
+    Class<MissionRecievedEvent> c ;
+
     @BeforeEach
     public void setUp(){
-
+        event = new MissionRecievedEvent(resault);
+        simlpeSub = new SimpleSubscriber();
     }
 
+
+    public void testSubscribEvent(){
+            MessageBrokerImpl ms = new MessageBrokerImpl();
+            ms.subscribeEvent(c,simlpeSub);
+    }
     @Test
     public void test(){
         //TODO: change this test and add more tests :)
